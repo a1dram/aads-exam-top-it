ca80417ae449bbfe27ee24527b9afaedc6e8a6c1
a1dram <aldram54@gmail.com>
2026-06-25 12:37:04 +0300

    fixed early file reading in main, now it closes before second

--- muraviev.alexander/U1/main.cpp
+++ muraviev.alexander/U1/main.cpp
@@ -13,35 +13,42 @@ int main(int argc, char** argv)
     return 1;
   }
 
-  std::ifstream inputFile;
-  std::ofstream outputFile;
-  std::istream* input = &std::cin;
-  std::ostream* output = &std::cout;
-  if (options.hasInput)
-  {
-    inputFile.open(options.inputName);
-    if (!inputFile)
-    {
-      return 2;
-    }
-    input = &inputFile;
-  }
-  if (options.hasOutput)
-  {
-    outputFile.open(options.outputName);
-    if (!outputFile)
-    {
-      return 2;
-    }
-    output = &outputFile;
-  }
-
   muraviev::PersonList persons;
   muraviev::initList(persons);
   size_t accepted = 0;
   size_t ignored = 0;
-  muraviev::readPersons(*input, persons, accepted, ignored);
-  muraviev::printPersons(*output, persons);
+
+  if (options.hasInput)
+  {
+    std::ifstream input(options.inputName);
+    if (!input)
+    {
+      muraviev::clearList(persons);
+      return 2;
+    }
+    muraviev::readPersons(input, persons, accepted, ignored);
+    input.close();
+  }
+  else
+  {
+    muraviev::readPersons(std::cin, persons, accepted, ignored);
+  }
+
+  if (options.hasOutput)
+  {
+    std::ofstream output(options.outputName);
+    if (!output)
+    {
+      muraviev::clearList(persons);
+      return 2;
+    }
+    muraviev::printPersons(output, persons);
+  }
+  else
+  {
+    muraviev::printPersons(std::cout, persons);
+  }
+
   std::cerr << accepted << ' ' << ignored << '\n';
   muraviev::clearList(persons);
   return 0;
