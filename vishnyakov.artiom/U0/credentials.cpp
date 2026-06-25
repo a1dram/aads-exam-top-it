b56a379011737a34ca308a3774b2eff0d9185973
ArtConion <artiom0408@mail.ru>
2026-06-25 10:47:05 +0300

    add crefentials header and inplementation

---
+++ vishnyakov.artiom/U0/credentials.cpp
@@ -0,0 +1,8 @@
+#include "credentials.hpp"
+#include <iostream>
+
+std::ostream & vishnaykov::out_credentials(std::ostream & out)
+{
+  return out << "vishnya.artiom";
+}
+
---
+++ vishnyakov.artiom/U0/credentials.hpp
@@ -0,0 +1,10 @@
+#ifndef CREDENTIALS_HPP
+#define CREDENTIALS_HPP
+#include <iosfwd>
+
+namespace vishnaykov
+{
+  std::ostream & vishnaykov::out_credentials(std::ostream & out);
+}
+#endif
+
